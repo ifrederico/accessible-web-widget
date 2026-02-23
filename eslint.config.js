e4cad8f8d26be9df72e072cdf0d5e2dab386fb63
Frederico Ribeiro <fr@tecompanytea.com>
2026-02-22 23:11:10 -0500

    fix: lint examples with browser globals

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,7 @@ export default [
   },
   js.configs.recommended,
   {
-    files: ['src/**/*.js', 'tests/**/*.js'],
+    files: ['src/**/*.js', 'tests/**/*.js', 'examples/**/*.js'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
